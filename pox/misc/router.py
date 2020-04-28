4c1d7480d32b76771defb3a12dac8db1442c93c0
Spencer McDonough <shmcdono@usc.edu>
2020-04-28 15:39:17 -0700

    gen_arp_reply()

--- pox/misc/router.py
+++ pox/misc/router.py
@@ -112,6 +112,25 @@ def validate_ip(rt_object, dpid, ip):
   return False
 
 ########################################## ARP functions ##########################################
+def generate_arp_reply(rt_object, dpid, packet, packet_in): 
+  arp_reply = arp() 
+  arp_reply.opcode = arp.REPLY 
+  arp_reply.hwsrc = packet.dst 
+  #Destination now is the source MAC address 
+  arp_reply.hwdst = packet.src 
+  arp_reply.protosrc = packet.payload.protodst 
+  arp_reply.protodst= packet.payload.protosrc 
+  eth = ethernet() 
+  eth.type = ethernet.ARP_TYPE 
+  eth.dst = rt_object.ip_to_mac[dpid][packet.payload.protosrc] 
+  eth.src = packet.dst 
+  eth.payload = arp_reply 
+  msg = of.ofp_packet_out() 
+  msg.data = eth.pack() 
+  action = of.ofp_action_output(port = packet_in.in_port) 
+  msg.actions.append(action) 
+  rt_object.connection.send(msg) 
+
 def arp_handler(rt_object, dpid, packet, packet_in):
   """
   Handles all incoming arp packets.
@@ -141,22 +160,24 @@ def arp_handler(rt_object, dpid, packet, packet_in):
   if packet.next.opcode == arp.REQUEST:
     # if destination ip is the router (default gw), generate arp response
     if (arp_dst_ip == rt_object.routing_table[dpid][get_subnet(rt_object, dpid, packet.payload.protosrc)]["router_interface"]):
-      arp_reply = arp()
-      arp_reply.opcode = arp.REPLY
-      arp_reply.hwsrc = packet.dst #Destination now is the source MAC address
-      arp_reply.hwdst = packet.src
-      arp_reply.protosrc = packet.payload.protodst
-      arp_reply.protodst= packet.payload.protosrc
-      eth = ethernet()
-      eth.type = ethernet.ARP_TYPE
-      eth.dst = rt_object.ip_to_mac[dpid][packet.payload.protosrc]
-      eth.src = packet.dst
-      eth.payload = arp_reply
-      msg = of.ofp_packet_out()
-      msg.data = eth.pack()
-      action = of.ofp_action_output(port = packet_in.in_port)
-      msg.actions.append(action)
-      rt_object.connection.send(msg)
+      generate_arp_reply(rt_object, dpid, packet, packet_in)
+      
+      # arp_reply = arp()
+      # arp_reply.opcode = arp.REPLY
+      # arp_reply.hwsrc = packet.dst #Destination now is the source MAC address
+      # arp_reply.hwdst = packet.src
+      # arp_reply.protosrc = packet.payload.protodst
+      # arp_reply.protodst= packet.payload.protosrc
+      # eth = ethernet()
+      # eth.type = ethernet.ARP_TYPE
+      # eth.dst = rt_object.ip_to_mac[dpid][packet.payload.protosrc]
+      # eth.src = packet.dst
+      # eth.payload = arp_reply
+      # msg = of.ofp_packet_out()
+      # msg.data = eth.pack()
+      # action = of.ofp_action_output(port = packet_in.in_port)
+      # msg.actions.append(action)
+      # rt_object.connection.send(msg)
 
       # DEBUG
       print("ARP_HANDLER(): Generate ARP Reply: answering MAC %s on port %d" % (rt_object.ip_to_mac[dpid][packet.payload.protosrc], packet_in.in_port))
@@ -168,22 +189,23 @@ def arp_handler(rt_object, dpid, packet, packet_in):
       print("ARP_HANDLER(): src ip: %s and dst ip: %s in same network." % (arp_src_ip, arp_dst_ip))
       # act_like_switch(rt_object, dpid, packet, packet_in)
       if(packet.dst == rt_object.routing_table[dpid][get_subnet(rt_object, dpid, arp_dst_ip)]["mac_interface"]):
-        arp_reply = arp()
-        arp_reply.opcode = arp.REPLY
-        arp_reply.hwsrc = packet.dst #Destination now is the source MAC address
-        arp_reply.hwdst = packet.src
-        arp_reply.protosrc = packet.payload.protodst
-        arp_reply.protodst= packet.payload.protosrc
-        eth = ethernet()
-        eth.type = ethernet.ARP_TYPE
-        eth.dst = rt_object.ip_to_mac[dpid][packet.payload.protosrc]
-        eth.src = packet.dst
-        eth.payload = arp_reply
-        msg = of.ofp_packet_out()
-        msg.data = eth.pack()
-        action = of.ofp_action_output(port = packet_in.in_port)
-        msg.actions.append(action)
-        rt_object.connection.send(msg)
+        generate_arp_reply(rt_object, dpid, packet, packet_in)
+        # arp_reply = arp()
+        # arp_reply.opcode = arp.REPLY
+        # arp_reply.hwsrc = packet.dst #Destination now is the source MAC address
+        # arp_reply.hwdst = packet.src
+        # arp_reply.protosrc = packet.payload.protodst
+        # arp_reply.protodst= packet.payload.protosrc
+        # eth = ethernet()
+        # eth.type = ethernet.ARP_TYPE
+        # eth.dst = rt_object.ip_to_mac[dpid][packet.payload.protosrc]
+        # eth.src = packet.dst
+        # eth.payload = arp_reply
+        # msg = of.ofp_packet_out()
+        # msg.data = eth.pack()
+        # action = of.ofp_action_output(port = packet_in.in_port)
+        # msg.actions.append(action)
+        # rt_object.connection.send(msg)
 
     # DEBUG
     else:
