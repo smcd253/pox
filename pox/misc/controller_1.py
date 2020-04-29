c8a856876987adbf4fa4c31b4929721c8dc90348
Spencer McDonough <shmcdono@usc.edu>
2020-04-28 17:09:26 -0700

    self.dpid

--- pox/misc/controller_1.py
+++ pox/misc/controller_1.py
@@ -115,7 +115,7 @@ class Tutorial (object):
       invoke router_handler and pass the object (i.e., self) and the packet and packet_in
     """
     # try switch only first
-    switch_handler(self, dpid, packet, packet_in)
+    switch_handler(self, self.dpid, packet, packet_in)
 
 
 
